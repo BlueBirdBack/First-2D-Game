2012da3d1e4b4ac0701f671e5fe71fbc58f34edf
Avery <avery@bluebirdback.com>
2025-03-07 07:22:02 +0800

    refactor: Clean up player script and improve code quality
    - Remove commented-out code and debug print statements
    - Add type hints to function parameters
    - Simplify player movement and animation logic

--- scripts/player.gd
+++ scripts/player.gd
@@ -12,12 +12,8 @@ func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 	hide()
 
-func _process(delta):
-	var fps = Engine.get_frames_per_second()
-	# print("FPS: ", fps)
-	# $Label.text = "FPS: " + str(int(fps))
-
-	var velocity = Vector2.ZERO
+func _process(delta: float) -> void:
+	var velocity: Vector2 = Vector2.ZERO
 
 	if Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
@@ -45,12 +41,7 @@ func _process(delta):
 		$AnimatedSprite2D.animation = "walk"
 		$AnimatedSprite2D.flip_v = velocity.y > 0
 
-	# if velocity.x < 0:
-	# 	$AnimatedSprite2D.flip_h = true
-	# else:
-	# 	$AnimatedSprite2D.flip_h = false
-
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(_body: Node2D) -> void:
 	hide()
 	hit.emit()
 	$CollisionShape2D.set_deferred("disabled", true)
