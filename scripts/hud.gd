0dc3c00e86b8cc091cb6185d1863c701f8103471
Avery <avery@bluebirdback.com>
2025-03-07 08:13:36 +0800

    refactor: Simplify FPS label visibility logic in HUD
    - Remove redundant FPS label hide/show calls
    - Ensure FPS label is visible by default
    - Clean up conditional FPS label display code

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -10,10 +10,9 @@ func _ready() -> void:
 	$StartButton.connect("pressed", _on_start_button_pressed)
 	$MessageTimer.connect("timeout", _on_message_timer_timeout)
 	if fps_label:
-		fps_label.hide() # Hide FPS label initially
+		fps_label.show()
 
-func _process(delta):
-	# UpdateFPSdisplayeverysecond
+func _process(delta: float) -> void:
 	fps_update_timer += delta
 	if fps_update_timer >= 1.0:
 		fps_update_timer = 0.0
@@ -24,8 +23,6 @@ func show_message(text: String) -> void:
 	$Message.text = text
 	$Message.show()
 	$MessageTimer.start()
-	if fps_label:
-		fps_label.hide() # Hide FPS when showing messages
 
 func show_game_over() -> void:
 	show_message("Game Over")
@@ -33,8 +30,6 @@ func show_game_over() -> void:
 
 	$Message.text = "Dodge the Creeps!"
 	$Message.show()
-	if fps_label:
-		fps_label.show() # Show FPS again after game over
 
 	await get_tree().create_timer(1.0).timeout
 	$StartButton.show()
@@ -45,8 +40,6 @@ func update_score(score: int) -> void:
 func _on_start_button_pressed() -> void:
 	$StartButton.hide()
 	start_game.emit()
-	if fps_label:
-		fps_label.show() # Show FPS when game starts
 
 func _on_message_timer_timeout() -> void:
 	$Message.hide()
